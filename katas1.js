9ab076fe76d397b248a8adf0b2a5a7c49af7e1bf
thuliobandeira <thuliao18@gmail.com>
2021-10-22 05:17:04 -0300

    tinha errado o katas,por ter feito ele responsivo,ajeitei e fiz do jeito que era para ser

--- katas1.js
+++ katas1.js
@@ -1,108 +1,111 @@
 function oneThroughTwenty() {
-  let retorno = [];
-  for(let i=1; i<=20;i++){
+  let retorno= [];
+  for (let i = 1; i <= 20; i++) {
     retorno.push(i)
-  }
-  return retorno;    
-}
-console.log(oneThroughTwenty())
-
-function evensToTwenty() {
-  let retorno = [];
-  for(let i =0;i<=20;i+=2){
-    retorno.push(i)
-  }
-  return retorno;
-}
-
-console.log(vensToTwenty())
-
-function oddsToTwenty() {
-  let retorno = [];
-  for(let i=1;i<=20;i+=2){
-    retorno.push(i)
-  }
-  return retorno;
-}
-
-console.log(oddsToTwenty())
-
-function multiplesOfFive(a) {
-  let retorno = [];
-  let mult = a*5;
-  for(let i=1;i<=mult;i++){
-    if(i%5===0){
-      retorno.push(i)
-    }
-  }
-  return retorno;
-}
-
-console.log(multiplesOfFive(a))
-
-function squareNumbers(num) {
-  let retorno = [];
-  let square =  num;
-  for(let i =1;i<=square;i++){
-    retorno.push(i*i)
   }
   return retorno
 }
+console.log(oneThroughTwenty())
 
-console.log(squareNumbers(num))
+ function evensToTwenty() {
+   let retorno = [];
+   for (let i = 1; i <=20;i ++){
+     if (i%2===0){
+       retorno.push(i)
+     }
+   }
+   return retorno
+ }
+console.log(evensToTwenty())
 
-function countingBackwards(a,b) {
+
+function oddsToTwenty(){
   let retorno = [];
-  for(let i=a*b;i>=0;i--){
-    retorno.push(i)
-  }
-  return retorno;
-}
-
-console.log(countingBackwards(a,b))
-
-function evenNumbersBackwards(a,b) {
-let retorno = [];
-for(let i = a*b;i>=0;i--){
-  if(i%2===0){
-    retorno.push(i)
+  for(let i =1; i<=20; i ++){
+    if(i%2 ===1){
+      retorno.push(i)
     }
   }
-  return retorno;
+  return retorno
 }
+console.log(oddsToTwenty())
 
+
+function multiplesOfFive(){
+  let retorno = [];
+  for(let i =5; i<=100;i++){
+    if(i%5==0 ){
+      retorno.push(i)
+    }
+  }
+  return retorno
+}
+console.log(multiplesOfFive())
+
+
+function squareNumbers() {
+  let retorno = [];
+  for(let i =1; i<=10;i++){
+    if(i*i==i**2){
+      retorno.push(i*i)
+    }
+  }
+  return retorno
+}
+console.log(squareNumbers())
+
+
+function countingBackwards() {
+  let retorno= [];
+  for(let i=20;i>=1;i--) {
+    retorno.push(i)
+  }
+  return retorno
+}
+console.log(countingBackwards())
+
+
+function evenNumbersBackwards() {
+  let retorno = [];
+  for(let i=20;i >= 1;i--){
+    if(i%2==0){
+      retorno.push(i)
+    }
+  }
+  return retorno
+}
 console.log(evenNumbersBackwards())
 
-function oddNumbersBackwards(a,b) {
-let retorno = [];
-for(let i =a*b ;i>=0;i--){
-  if(i%2===1){
-    retorno.push(i)
+
+function oddNumbersBackwards() {
+  let retorno = [];
+  for(let i=20; i>=1; i--){
+    if(i%2==1){
+      retorno.push(i)
+    }
   }
- }
- return retorno;
+  return retorno
 }
+console.log(oddNumbersBackwards())
 
-console.log(oddNumbersBackwards(a,b))
-
-function multiplesOfFiveBackwards(a,b) {
-let retorno = [];
-for(let i=a*b;i>=1;i--){
-  if(i%5===0){
-    retorno.push(i)
+function multiplesOfFiveBackwards() {
+  let retorno = [];
+  for(let i=100;i>=5;i-- ){
+    if(i%5==0){
+      retorno.push(i)
+    }
   }
- }
- return retorno;
+  return retorno
 }
-
 console.log(multiplesOfFiveBackwards())
 
-function squareNumbersBackwards(a,b) {
-let retorno = [];
-for(let i =a*b;i>=1;i--){
-  retorno.push(i*i)
- }
- return retorno;
+function squareNumbersBackwards(){
+  let retorno = [];
+  for(let i=10;i>=1;i--){
+    if(i*i==i**2){
+      retorno.push(i*i)
+    }
+  }
+  return retorno
 }
-
-console.log(squareNumbersBackwards(a,b))
+console.log(squareNumbersBackwards())
